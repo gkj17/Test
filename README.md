cf9bc1d34762c538e6d5271cda4184414e6f1e74
gkj17 <guan_kejian@163.com>
2023-09-20 08:26:39 +0800

    删除无用的

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is a example to show the problem in paging3 that a manual refresh will cause the list to jump to 3*loadSize (default)
+这是用于测试的项目
--- app/src/main/java/cn/guankejian/test/bean/UIModel.kt
+++
@@ -1,8 +0,0 @@
-package cn.guankejian.test.bean
-
-sealed class UIModel{
-    data class IconItem(val item: MZTag):UIModel()
-    data class HeaderItem(val item: String):UIModel()
-    data class SepItem(val item:String):UIModel()
-    data class FooterItem(val item:String):UIModel()
-}
